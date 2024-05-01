2c4e467817b3aaaf82069e8ecc5fcc222f124384
mor-codes <morcodes@proton.me>
2024-05-02 00:59:02 +0700

    Fix value never read

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
 import CodeSnapshot from './components/CodeSnapshot'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className='w-100'>
