66b2ca5b1ba019849ade102d3ead342381a96ba2
mor-codes <morcodes@proton.me>
2024-05-02 01:53:46 +0700

    CodeBadge use sm

--- src/components/CodeBadge.tsx
+++ src/components/CodeBadge.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function CodeBadge({ children }: { children: ReactNode }) {
     return (
-        <div className="px-3 py-1 rounded-full bg-white text-black text-xs">
+        <div className="px-3 py-1 rounded-full bg-white text-black text-sm">
             {children}
         </div>
     )
